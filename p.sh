5bb2d8df4296db6b20322bbc4f94d901838fffc8
cotswoldsmaker <mark.allan.bailey@gmail.com>
2023-01-02 21:52:44 +0000

    p.sh update

--- p.sh
+++ p.sh
@@ -4,7 +4,7 @@ echo "Pushing documentation to gh-pages...";
 docker compose --file ./docs/Docker-compose_gh_deploy.yml up
 
 #echo "Running commit - \"message $1\"";
-git commit --all -m "$1"
-git push origin main
+#git commit --all -m "$1"
+#git push origin main
 
 echo "Documents done";
