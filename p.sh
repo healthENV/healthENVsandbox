f47de6c4c410c06944425a110d22289cc014eae7
cotswoldsmaker <mark.allan.bailey@gmail.com>
2023-01-02 21:46:16 +0000

    added script to push files and docs

--- p.sh
+++ p.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "Pushing documentation to gh-pages...";
-docker compose --file ./docs/Docker-compose_gh_deploy.yml up
+#docker compose --file ./docs/Docker-compose_gh_deploy.yml up
 
 echo "Running commit - \"message $1\"";
 git commit --all -m "$1"
