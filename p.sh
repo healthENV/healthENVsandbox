65b5cbe2ece7e206ccb29f35e2b7d699dec99636
cotswoldsmaker <mark.allan.bailey@gmail.com>
2023-01-03 16:20:11 +0000

    some playing around with gh-deploy

--- p.sh
+++ p.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
-
+# TODO: #2 It would be great to have the gh-deploy working within GitHub actions from a Docker container. (difficult to do so far)
 echo "Pushing documentation to gh-pages...";
-docker compose --file ./docs/Docker-compose_gh_deploy.yml up
+#docker compose --file ./docs/Docker-compose_gh_deploy.yml up
 
-#echo "Running commit - \"message $1\"";
-#git commit --all -m "$1"
-#git push origin main
+echo "Running commit - \"message $1\"";
+git commit --all -m "$1"
+git push origin main
 
 echo "Documents done";
